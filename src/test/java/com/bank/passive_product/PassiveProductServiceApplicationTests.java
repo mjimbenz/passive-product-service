a6ed4446f88d51724ba30310da18f8dd21d063f5
mjimbenz <MiguelAutberto.JimenezBenzol@emeal.nttdata.com>
2026-03-21 18:17:44 -0500

    chore(init): create spring boot base project

---
+++ src/test/java/com/bank/passive_product/PassiveProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.passive_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassiveProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
