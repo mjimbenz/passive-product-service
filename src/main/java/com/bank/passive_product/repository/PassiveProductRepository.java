11aa04e539f964a3b2fd328599cf83721325977d
mjimbenz <MiguelAutberto.JimenezBenzol@emeal.nttdata.com>
2026-03-21 22:07:03 -0500

    feat(repository): add reactive methods for soft delete and active-only queries - Added findByActiveTrue() - Added findByIdAndActiveTrue(id) - Repository now supports filtering active entities for findAll and findById

--- src/main/java/com/bank/passive_product/repository/PassiveProductRepository.java
+++ src/main/java/com/bank/passive_product/repository/PassiveProductRepository.java
@@ -2,6 +2,12 @@ package com.bank.passive_product.repository;
 
 import com.bank.passive_product.model.PassiveProductEntity;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface PassiveProductRepository extends ReactiveMongoRepository<PassiveProductEntity, String> {
+    Flux<PassiveProductEntity> findByCustomerId(String customerId);
+    Flux<PassiveProductEntity> findByActiveTrue();
+
+    Mono<PassiveProductEntity> findByIdAndActiveTrue(String id);
 }
